5b97c2c3b4ed057d2d63c0ef87eb52e82d3600c5
KimiJok1 <kimi.jokelainen@gmail.com>
2023-05-09 10:46:35 +0300

    Format changes

--- Taitaja2023-Finaali/Assets/Scripts/Environment/Parallax.cs
+++ Taitaja2023-Finaali/Assets/Scripts/Environment/Parallax.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     //Script taken from https://vionixstudio.com/2022/09/22/unity-parallax-background/
-    [SerializeField] private GameObject sky;
-    [SerializeField] private Camera your_camera;
-    [SerializeField] private float parallax_value;
+    [SerializeField] private GameObject object;
+    [SerializeField] private Camera camera;
+    [SerializeField] private float parallaxValue;
 
-    private Vector3 startposition;
+    private Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        startposition = sky.transform.position;      
+        startPosition = object.transform.position;      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 relative_pos = your_camera.transform.position * parallax_value;
-        relative_pos.z = startposition.z;
-        sky.transform.position = startposition + relative_pos;
+        Vector3 relative_pos = camera.transform.position * parallaxValue;
+        relative_pos.z = startPosition.z;
+        object.transform.position = startPosition + relative_pos;
     }
 }
