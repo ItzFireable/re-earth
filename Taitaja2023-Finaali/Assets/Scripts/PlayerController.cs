ea6442bd782122df0caa66cfc20735bfb74718ca
KimiJok1 <kimi.jokelainen@gmail.com>
2023-05-09 15:08:40 +0300

    Fixed melee animations

--- Taitaja2023-Finaali/Assets/Scripts/PlayerController.cs
+++ Taitaja2023-Finaali/Assets/Scripts/PlayerController.cs
@@ -63,11 +63,14 @@ public class PlayerController : MonoBehaviour
 
         // Play animation and wait for it to finish
         animator.SetTrigger("Attack" + anim);
+
+        // Wait until animation starts
+        yield return new WaitWhile(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0);
         transform.position = new Vector3(transform.position.x + 1.25f, transform.position.y, transform.position.z);
         cameraPoint.transform.localPosition = new Vector3(-1.25f, 0f, 0f);
 
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length - 0.2f);
-
+        // Wait for the animation length
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
         transform.position = new Vector3(transform.position.x - 1.25f, transform.position.y, transform.position.z);
         cameraPoint.transform.localPosition = new Vector3(0f, 0f, 0f);
 
