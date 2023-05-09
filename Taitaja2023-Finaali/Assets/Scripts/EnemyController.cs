745c09ed58dee990503a74b3fe59a1bab8de904e
KimiJok1 <kimi.jokelainen@gmail.com>
2023-05-09 16:47:37 +0300

    Documentation

--- Taitaja2023-Finaali/Assets/Scripts/EnemyController.cs
+++ Taitaja2023-Finaali/Assets/Scripts/EnemyController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-
+    // Serialized fields for the enemy
     [SerializeField] private GameObject player;
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject realPosition;
 
+    // Health properties
     [SerializeField] private int maxHealth;
     private int health;
+
+    // Facig direction
     private bool facing;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get health & player
         health = maxHealth;
         player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -23,20 +27,27 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update facing direction
         faceToPlayer();
     }
 
-    //Checks if enemy is looking at player and changes it's facing side
+    // Function to check if enemy is facing player or not
     void faceToPlayer()
     {
+        // Get positional difference for player and enemy
         float difference = realPosition.transform.position.x - player.transform.position.x;
+
+        // Checks if it is already facing player
         if((difference < 0 && facing) || (difference > 0 && !facing))
         {
+            // Flip enemy and its position
             transform.localScale = new Vector3(transform.localScale.x * -1f, 1f, 1f);
             if(facing)
                 transform.position = new Vector3(transform.position.x + 1.75f, transform.position.y, 0f);
             else
                 transform.position = new Vector3(transform.position.x - 1.75f, transform.position.y, 0f);
+
+            // Flips the facing direction
             facing = !facing;
         }
     }
