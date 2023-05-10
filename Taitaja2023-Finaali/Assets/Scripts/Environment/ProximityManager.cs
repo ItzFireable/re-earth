c998b69207d7eb99181d343b6ecedb45f5da1d1d
KimiJok1 <kimi.jokelainen@gmail.com>
2023-05-10 11:04:48 +0300

    Check for enemy dead status

--- Taitaja2023-Finaali/Assets/Scripts/Environment/ProximityManager.cs
+++ Taitaja2023-Finaali/Assets/Scripts/Environment/ProximityManager.cs
@@ -21,22 +21,33 @@ public class ProximityManager : MonoBehaviour
             // Get the magnitude of the child
             Vector3 offset = child.position - player.position;
             float sqrLen = offset.sqrMagnitude;
-            
-            // If nearest object exists
-            if (nearestObject)
-            {
-                // Get the magnitude of the nearest object
-                Vector3 nearestOffset = nearestObject.position - player.position;
-                float nearestSqlLen = offset.sqrMagnitude;
 
-                // Compare the magnitudes and update nearestobject accordingly
-                nearestObject = sqrLen < nearestSqlLen ? child : nearestObject;
-                isInRange = (nearestSqlLen < sqrLen ? nearestSqlLen : sqrLen) < distance * distance;
-            }
-            else
+            if (!child.GetComponent<EnemyController>().isDead)
             {
-                // Update nearest object to be current child
-                nearestObject = child;
+                // If nearest object exists
+                if (nearestObject)
+                {
+                    if (!nearestObject.GetComponent<EnemyController>().isDead)
+                    {
+                        // Get the magnitude of the nearest object
+                        Vector3 nearestOffset = nearestObject.position - player.position;
+                        float nearestSqlLen = offset.sqrMagnitude;
+
+                        // Compare the magnitudes and update nearestobject accordingly
+                        nearestObject = sqrLen < nearestSqlLen ? child : nearestObject;
+                        isInRange = (nearestSqlLen < sqrLen ? nearestSqlLen : sqrLen) < distance * distance;
+                    }
+                    else
+                    {
+                        // Update nearest object to be current child
+                        nearestObject = child;
+                    }
+                }
+                else
+                {
+                    // Update nearest object to be current child
+                    nearestObject = child;
+                }
             }
         }
     }
